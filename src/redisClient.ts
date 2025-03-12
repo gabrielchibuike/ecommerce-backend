93e42db11a7ca93117e903ad108d1ce9a1ce56f9
GabbySoft <gabrielnwagu2002@gmail.com>
2025-03-12 02:42:04 +0100

    implemented redis

--- src/redisClient.ts
+++ src/redisClient.ts
@@ -1,10 +1,8 @@
 import Redis from "ioredis";
 import { logger } from "./utils/logger";
 
-const redis = new Redis({
-  //   host: process.env.REDIS_HOST || "127.0.0.1",
-  port: Number(process.env.REDIS_URL),
-  retryStrategy: (times) => Math.min(times * 50, 2000), // Reconnect strategy
+const redis = new Redis(process.env.REDIS_URL || "redis://127.0.0.1:6379", {
+  // retryStrategy: (times) => Math.min(times * 50, 2000), // Reconnect strategy
 });
 
 redis.on("connect", () => logger.info("Connected to Redis"));
