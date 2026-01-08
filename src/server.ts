5e4290beb9915be8e88d143ee407033013cd5fbc
GabbySoft <102861818+gabrielchibuike@users.noreply.github.com>
2026-01-08 08:03:08 +0100

    Fix CORS origin URL by removing trailing slash

--- src/server.ts
+++ src/server.ts
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://store-frontend-sage-xi.vercel.app/",
+    origin: "https://store-frontend-sage-xi.vercel.app",
     // origin: "http://localhost:3000",
     credentials: true,
   })
