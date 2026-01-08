11fda372abd4b2192c294b1915921f5e618f242a
Gabby <gabrielnwagu2002@gmail.com>
2026-01-08 07:57:29 +0100

    new features added

--- src/server.ts
+++ src/server.ts
@@ -32,7 +32,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://store-frontend-sage-xi.vercel.app/",
+    // origin: "http://localhost:3000",
     credentials: true,
   })
 );
